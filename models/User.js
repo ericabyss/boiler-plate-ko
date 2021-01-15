a59df79c7ddde7779f74956407a2d7a1ff5b1140
ericabyss <erica051746@naver.com>
2021-01-15 14:11:27 +0900

    cm0115

--- models/User.js
+++ models/User.js
@@ -8,32 +8,32 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        trim: true,
-        unique: 1
+        trim: true, //잘라줌
+        unique: 1 //중복 방지
     },
     password: {
         type: String,
         minlength: 5
     },
     lastname: {
-        type:String,
+        type: String,
         maxlength: 50
     },
     role: {//관리자, 유저
         type: Number,
         default: 0
     },
-    image: String,
+    image: String, //이렇게도 타입 지정 가능
     token:{ //유효성
         type: String
 
     },
-    tokenExp: {
+    tokenExp: {//토큰 사용기간
         type: Number
     }
 
 })
 
-const User = mongoose.model('User',userSchema)
+const User = mongoose.model('User',userSchema)//스키마를 모델로 감싸줌 , 모델의 이름 : User
 
-module.exports = { User }
+module.exports = { User } //이 모델을 다른곳에서도 쓸 수 있게
