281561c76a667557fc9a272a99f649df055780b7
ericabyss <erica051746@naver.com>
2021-01-18 01:21:45 +0900

    1

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require('express') //express module을 가져옴
+const express = require('express')  //express module을 가져옴
 const app = express() //function을 이용하여 새로운 express app을 만들고
-const port = 5000  //백서버로 둘 포트
+const port = 5000 //백서버로 둘 포트
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://john:<password>@boilerplate.p8jgc.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+const mongoose = require('mongoose')//mongoose연결
+mongoose.connect('mongodb+srv://john:<abcd1234>@boilerplate.p8jgc.mongodb.net/<boilerplate>?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err))//잘 연결이 안되었을때
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!sukyoung') //출력
-})
+  app.get('/', (req, res) => res.send('Hello World!')) //루트 디렉토리에서 출력, 간단한 라우트
 
-app.listen(port, () => {
-  console.log('Example app listening at http://localhost:${port}!') //app실행
-})
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))//설정 포트에서 앱을 실행
