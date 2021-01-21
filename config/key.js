473d7259e8aecd00e4772baf6b4fb628a0ebb3c5
ericabyss <erica051746@naver.com>
2021-01-21 17:09:16 +0900

    0121_1

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
-if(proccess.env.NODE_ENV === 'production'){// proccess.env.NODE_ENV 환경변수
+if(process.env.NODE_ENV === 'production') {// proccess.env.NODE_ENV 환경변수
     module.exports = require('./prod');
 } else{
     module.exports = require('./dev');
